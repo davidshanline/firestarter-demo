6a62073c3c0f343cb6dc0ed2b1df5684e2ad5352
davidshanline <davidshanline+programming@gmail.com>
2020-06-11 10:53:33 -0400

    firebase integration for google and email/pass signin

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBeLAOX-iAVHoVyU6GLxsdGgOkyl_l26bI",
+    authDomain: "firestarter-demo-560fe.firebaseapp.com",
+    databaseURL: "https://firestarter-demo-560fe.firebaseio.com",
+    projectId: "firestarter-demo-560fe",
+    storageBucket: "firestarter-demo-560fe.appspot.com",
+    messagingSenderId: "1003146717089",
+    appId: "1:1003146717089:web:5b1bfbdc877ed874d98de0",
+    measurementId: "G-QNPQXK1PHS"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBeLAOX-iAVHoVyU6GLxsdGgOkyl_l26bI",
+    authDomain: "firestarter-demo-560fe.firebaseapp.com",
+    databaseURL: "https://firestarter-demo-560fe.firebaseio.com",
+    projectId: "firestarter-demo-560fe",
+    storageBucket: "firestarter-demo-560fe.appspot.com",
+    messagingSenderId: "1003146717089",
+    appId: "1:1003146717089:web:5b1bfbdc877ed874d98de0",
+    measurementId: "G-QNPQXK1PHS"
+  }
 };
 
 /*
